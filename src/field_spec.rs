74ab6c71624d3f2f33a11fae9cb25914949d4b7b
Johannes Kreutle <johannes.kreutle@web.de>
2023-04-17 17:44:58 +0200

    Added crate bytes

--- src/field_spec.rs
+++ src/field_spec.rs
@@ -1,5 +1,9 @@
 use std::mem;
 
+// Primary Subfield
+// | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | bit
+// |SF1|SF2|SF3|SF4|SF5|SF6|SF7| FX| subfield
+//
 // The attributes in structs have Network Byte Order in Big Endian
 #[repr(packed(1))]
 //#[derive(Debug, PartialEq)]
@@ -37,23 +41,49 @@ impl FieldSpec {
     }
 
     /*
-     * Set bit. checkme
+     * Set subfield from bit right to left. [8..1]
      */
-    pub fn set_bit(&mut self, position: u8, state: bool) {
-        let mask = 0xff | (state as u8) << position;
-        self.fspec = self.fspec & mask;
+    pub fn set_subfield(&mut self, bit: u8, state: bool) {
+        if bit < 9 && bit > 0 {
+            if state {
+                let mask = 2u8.pow(bit as u32 - 1);
+                self.fspec = self.fspec | mask;
+            } else {
+                let mask = 0xff - 2u8.pow(bit as u32 - 1); // 1111 1111 subtract
+                self.fspec = self.fspec & mask;
+            }
+        }
     }
 
     /*
-     * Get bit. checkme
+     * Get subfield from bit right to left. [8..1]
      */
-    pub fn get_bit(&self, position: u8) -> bool {
-        let mask = 0x1 << position;
-        return self.fspec & mask == 1;
+    pub fn get_subfield(&self, bit: u8) -> bool {
+        if bit < 9 && bit > 0 {
+            let mask = 2u8.pow(bit as u32 - 1);
+            let result = self.fspec & mask;
+            return result == 1;
+        }
+        return false;
     }
 
     /*
      * Message length in memory.
      */
     pub const MESSAGE_LENGTH: usize = mem::size_of::<Self>();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_subfield() {
+        let mut field = FieldSpec::new();
+
+        field.set_subfield(3, true);
+        let result = field.get_subfield(3);
+
+        assert_eq!(true, true);
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,31 @@ pub mod cat_error;
 pub mod field_spec;
 pub mod tools;
 
+use bytes::Bytes;
+use cat34::Cat34Message;
+use cat_error::Cat34Error;
+
+/**
+ * Encode into CAT34 byte stream
+ */
+fn encode(message: &Cat34Message) -> Result<Bytes, Cat34Error> {
+
+    let bytes = Bytes::from(vec![1, 2, 3]);
+
+    Ok(bytes)
+ //   Err(Cat34Error::I034AllValid)
+}
+
+/**
+ * Decode from CAT34 byte stream
+ */
+fn decode(bytes: Bytes) -> Result<Cat34Message, Cat34Error> {
+    if bytes.len() > 0 {
+        return Ok(Cat34Message::new(cat34::MessageType::NorthMarker));
+    }
+
+    Err(Cat34Error::I034_000Invalid)
+}
 
 #[cfg(test)]
 mod tests {
